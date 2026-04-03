e589d2ec4ffad02feb3138a72a2d02e788b6468b
Yanel <yanelalmonte@gmail.com>
2026-04-03 19:31:55 -0400

    La bala destruye al enemigo o se destruye ella si choca con el muro

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -2,6 +2,16 @@ extends Area2D
 
 var speed = 200
 
+func _ready():
+	await get_tree().create_timer(5.0).timeout
+	queue_free()
+
 func _physics_process(delta):
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * speed * delta
+
+func _on_body_entered(body):
+	if body.is_in_group("Enemies"):
+		body.queue_free()
+	elif body.is_in_group("Walls"):
+		queue_free()
