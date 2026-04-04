423dece410b0ac1e74eeb62627c6c9cbc02e551e
Yanel <yanelalmonte@gmail.com>
2026-04-03 22:41:07 -0400

    Agrega animación de daño al enemigo y mejora la lógica de movimiento

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -13,7 +13,10 @@ func _physics_process(delta):
 func _on_body_entered(body):
 	if body.is_in_group("Enemies"):
 		_explosion()
-		body.queue_free()
+		if body.has_method("take_damage"):
+			body.take_damage()
+		
+		
 	elif body.is_in_group("Walls"):
 		_explosion()
 
